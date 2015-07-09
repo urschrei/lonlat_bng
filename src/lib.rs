63a47b9f83ddc8ecd4f1a6d028f0d2c0e96d5799
Stephan Hügel <urschrei@gmail.com>
2015-07-09 23:21:00 +0100

    More accurate arithmetic
    Use f64 in all conversion functions
    Use integer exponentiation where appropriate

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,8 @@
 //! assert_eq!((516276, 173141), lonlat_bng::convert(-0.32824866, 51.44533267));
 //!```
 
-use std::f32::consts;
+use std::{f32, f64};
+// use std::f32::consts;
 use std::mem;
 use std::slice;
 use std::thread::{self, JoinHandle};
@@ -72,7 +73,7 @@ impl Array {
 }
 
 // http://stackoverflow.com/a/28124775/155423
-fn round(x: f32) -> f32 {
+fn round(x: f64) -> f64 {
     let y = x.floor();
     if x == y {
         x
@@ -91,7 +92,7 @@ fn round(x: f32) -> f32 {
 /// assert_eq!((516276, 173141), convert_bng(-0.32824866, 51.44533267));
 #[allow(non_snake_case)]
 #[no_mangle]
-pub extern fn convert_bng(input_lon: f32, input_lat: f32) -> (i32, i32) {
+pub extern fn convert_bng(input_lon: f64, input_lat: f64) -> (i32, i32) {
     // match is restricted to the UK bounding box
     match input_lon {
         -6.379880...1.768960 => input_lon,
@@ -101,102 +102,102 @@ pub extern fn convert_bng(input_lon: f32, input_lat: f32) -> (i32, i32) {
         49.871159...55.811741 => input_lat,
         _ => panic!("Out of bounds! Latitude must be between -90.00 and 90.00: {:?}", input_lat)
     };
-    let pi: f32 = consts::PI;
+    let pi: f64 = f64::consts::PI;
     //Convert input to degrees
-    let lat_1: f32 = input_lat * pi / 180.;
-    let lon_1: f32 = input_lon * pi / 180.;
+    let lat_1: f64 = input_lat * pi / 180.;
+    let lon_1: f64 = input_lon * pi / 180.;
     // The GSR80 semi-major and semi-minor axes used for WGS84 (m)
-    let a_1: f32 = 6378137.;
-    let b_1: f32 = 6356752.3141;
+    let a_1: f64 = 6378137.;
+    let b_1: f64 = 6356752.3141;
     // The eccentricity (squared) of the GRS80 ellipsoid
-    let e2_1: f32 = 1. - (b_1.powf(2.)) / (a_1.powf(2.));
+    let e2_1: f64 = 1. - (b_1.powi(2)) / (a_1.powi(2));
     // Transverse radius of curvature
-    let nu_1: f32 = a_1 / (1. - e2_1 * lat_1.sin().powf(2.)).sqrt();
+    let nu_1: f64 = a_1 / (1. - e2_1 * lat_1.sin().powi(2)).sqrt();
     // Third spherical coordinate is 0, in this case
-    let H: f32 = 0.;
-    let x_1: f32 = (nu_1 + H) * lat_1.cos() * lon_1.cos();
-    let y_1: f32 = (nu_1 + H) * lat_1.cos() * lon_1.sin();
-    let z_1: f32 = ((1. - e2_1) * nu_1 + H) * lat_1.sin();
+    let H: f64 = 0.;
+    let x_1: f64 = (nu_1 + H) * lat_1.cos() * lon_1.cos();
+    let y_1: f64 = (nu_1 + H) * lat_1.cos() * lon_1.sin();
+    let z_1: f64 = ((1. - e2_1) * nu_1 + H) * lat_1.sin();
 
     // Perform Helmert transform (to go between Airy 1830 (_1) and GRS80 (_2))
-    let small: f32 = 10.;
-    let cst: f32 = 20.4894;
-    let s: f32 =  cst * small.powf(-6.);
+    let small: f64 = 10.;
+    let cst: f64 = 20.4894;
+    let s: f64 =  cst * small.powi(-6);
     // The translations along x, y, z axes respectively
-    let tx: f32 = -446.448;
-    let ty: f32 = 125.157;
-    let tz: f32 = -542.060;
+    let tx: f64 = -446.448;
+    let ty: f64 = 125.157;
+    let tz: f64 = -542.060;
     // The rotations along x, y, z respectively, in seconds
-    let rxs: f32 = -0.1502;
-    let rys: f32 = -0.2470;
-    let rzs: f32 = -0.8421;
+    let rxs: f64 = -0.1502;
+    let rys: f64 = -0.2470;
+    let rzs: f64 = -0.8421;
     // In radians
-    let rx: f32 = rxs * pi / (180. * 3600.);
-    let ry: f32 = rys * pi / (180. * 3600.);
-    let rz: f32 = rzs * pi / (180. * 3600.);
-    let x_2: f32 = tx + (1. + s) * x_1 + -rz * y_1 + ry * z_1;
-    let y_2: f32 = ty + rz * x_1 + (1. + s) * y_1 + -rx * z_1;
-    let z_2: f32 = tz + -ry * x_1 + rx * y_1 + (1. + s) * z_1;
+    let rx: f64 = rxs * pi / (180. * 3600.);
+    let ry: f64 = rys * pi / (180. * 3600.);
+    let rz: f64 = rzs * pi / (180. * 3600.);
+    let x_2: f64 = tx + (1. + s) * x_1 + -rz * y_1 + ry * z_1;
+    let y_2: f64 = ty + rz * x_1 + (1. + s) * y_1 + -rx * z_1;
+    let z_2: f64 = tz + -ry * x_1 + rx * y_1 + (1. + s) * z_1;
 
     // The GSR80 semi-major and semi-minor axes used for WGS84 (m)
-    let a: f32 = 6377563.396;
-    let b: f32 = 6356256.909;
+    let a: f64 = 6377563.396;
+    let b: f64 = 6356256.909;
     // The eccentricity of the Airy 1830 ellipsoid
-    let e2: f32 = 1. - b.powf(2.) / a.powf(2.);
-    let p: f32 = (x_2.powf(2.) + y_2.powf(2.)).sqrt();
+    let e2: f64 = 1. - b.powi(2) / a.powi(2);
+    let p: f64 = (x_2.powi(2) + y_2.powi(2)).sqrt();
     // Initial value
-    let mut lat: f32 = z_2.atan2((p * (1. - e2)));
-    let mut latold: f32 = 2. * pi;
+    let mut lat: f64 = z_2.atan2((p * (1. - e2)));
+    let mut latold: f64 = 2. * pi;
     // this is cheating, but not sure how else to initialise nu
-    let mut nu: f32 = 1.;
+    let mut nu: f64 = 1.;
     // Latitude is obtained by iterative procedure
-    while (lat - latold).abs() > small.powf(-16.) {
+    while (lat - latold).abs() > small.powi(-16) {
         mem::swap(&mut lat, &mut latold);
-        nu = a / (1. - e2 * latold.sin().powf(2.)).sqrt();
+        nu = a / (1. - e2 * latold.sin().powi(2)).sqrt();
         lat = (z_2 + e2 * nu * latold.sin()).atan2(p);
     };
-    let lon: f32 = y_2.atan2(x_2);
+    let lon: f64 = y_2.atan2(x_2);
     // Scale factor on the central meridian
-    let F0: f32 = 0.9996012717;
+    let F0: f64 = 0.9996012717;
     // Latitude of true origin (radians)
-    let lat0: f32 = 49. * pi / 180.;
+    let lat0: f64 = 49. * pi / 180.;
     // Longtitude of true origin and central meridian (radians)
-    let lon0: f32 = -2. * pi / 180.;
+    let lon0: f64 = -2. * pi / 180.;
     // Northing & easting of true origin (m)
-    let N0: f32 = -100000.;
-    let E0: f32 = 400000.;
-    let n: f32 = (a - b) / (a + b);
+    let N0: f64 = -100000.;
+    let E0: f64 = 400000.;
+    let n: f64 = (a - b) / (a + b);
     // Meridional radius of curvature
-    let rho: f32 = a * F0 * (1. - e2) * (1. - e2 * lat.sin().powf(2.)).powf(-1.5);
-    let eta2: f32 = nu * F0 / rho - 1.;
+    let rho: f64 = a * F0 * (1. - e2) * (1. - e2 * lat.sin().powi(2)).powf(-1.5);
+    let eta2: f64 = nu * F0 / rho - 1.;
 
-    let M1: f32 = (1. + n + (5. / 4.) * n.powf(2.)
-        + (5. / 4.) * n.powf(3.)) * (lat - lat0);
-    let M2: f32 = (3. * n + 3. * n.powf(2.) + (21. / 8.)
-        * n.powf(3.)) * (lat - lat0).sin() * (lat + lat0).cos();
-    let M3: f32 = ((15. / 8.) * n.powf(2.) + (15. / 8.)
-        * n.powf(3.)) * (2. * (lat-lat0)).sin() * (2. * (lat + lat0)).cos();
-    let M4: f32 = (35. / 24.) * n.powf(3.) * (3. * (lat - lat0)).sin()
+    let M1: f64 = (1. + n + (5. / 4.) * n.powi(2)
+        + (5. / 4.) * n.powi(3)) * (lat - lat0);
+    let M2: f64 = (3. * n + 3. * n.powi(2) + (21. / 8.)
+        * n.powi(3)) * (lat - lat0).sin() * (lat + lat0).cos();
+    let M3: f64 = ((15. / 8.) * n.powi(2) + (15. / 8.)
+        * n.powi(3)) * (2. * (lat-lat0)).sin() * (2. * (lat + lat0)).cos();
+    let M4: f64 = (35. / 24.) * n.powi(3) * (3. * (lat - lat0)).sin()
         * (3. * (lat + lat0)).cos();
-    let M: f32 = b * F0 * (M1 - M2 + M3 - M4);
+    let M: f64 = b * F0 * (M1 - M2 + M3 - M4);
 
-    let I: f32 = M + N0;
-    let II: f32 = nu * F0 * lat.sin() * lat.cos() / 2.;
-    let III: f32 = nu * F0 * lat.sin() * lat.cos().powf(3.)
-        * (5. - lat.tan().powf(2.) + 9. * eta2) / 24.;
-    let IIIA: f32 = nu * F0 * lat.sin() * lat.cos().powf(5.)
-        * (61. - 58. * lat.tan().powf(2.)
-        + lat.tan().powf(4.)) / 720.;
-    let IV: f32 = nu * F0 * lat.cos();
-    let V: f32 = nu * F0 * lat.cos().powf(3.)
-        * (nu / rho - lat.tan().powf(2.)) / 6.;
-    let VI: f32 = nu * F0 * lat.cos().powf(5.)
-        * (5. - 18. * lat.tan().powf(2.) + lat.tan().powf(4.)
-        + 14. * eta2 - 58. * eta2 * lat.tan().powf(2.)) / 120.;
-    let N: f32 = I + II * (lon - lon0).powf(2.)
-        + III * (lon - lon0).powf(4.) + IIIA * (lon - lon0).powf(6.);
-    let E: f32 = E0 + IV * (lon - lon0) + V * (lon - lon0).powf(3.)
-        + VI * (lon - lon0).powf(5.);
+    let I: f64 = M + N0;
+    let II: f64 = nu * F0 * lat.sin() * lat.cos() / 2.;
+    let III: f64 = nu * F0 * lat.sin() * lat.cos().powi(3)
+        * (5. - lat.tan().powi(2) + 9. * eta2) / 24.;
+    let IIIA: f64 = nu * F0 * lat.sin() * lat.cos().powi(5)
+        * (61. - 58. * lat.tan().powi(2)
+        + lat.tan().powi(4)) / 720.;
+    let IV: f64 = nu * F0 * lat.cos();
+    let V: f64 = nu * F0 * lat.cos().powi(3)
+        * (nu / rho - lat.tan().powi(2)) / 6.;
+    let VI: f64 = nu * F0 * lat.cos().powi(5)
+        * (5. - 18. * lat.tan().powi(2) + lat.tan().powi(4)
+        + 14. * eta2 - 58. * eta2 * lat.tan().powi(2)) / 120.;
+    let N: f64 = I + II * (lon - lon0).powi(2)
+        + III * (lon - lon0).powi(4) + IIIA * (lon - lon0).powi(6);
+    let E: f64 = E0 + IV * (lon - lon0) + V * (lon - lon0).powi(3)
+        + VI * (lon - lon0).powi(5);
     (round(E) as i32, round(N) as i32)
 }
 
@@ -210,17 +211,17 @@ pub extern fn convert_bng(input_lon: f32, input_lat: f32) -> (i32, i32) {
 /// assert_eq!((-0.328248, 51.44534), convert_lonlat(516276, 173141)));
 #[allow(non_snake_case)]
 #[no_mangle]
-pub extern fn convert_lonlat(input_e: i32, input_n: i32) -> (f32, f32) {
-    let pi: f32 = consts::PI;
+pub extern fn convert_lonlat(input_e: i32, input_n: i32) -> (f64, f64) {
+    let pi: f64 = f64::consts::PI;
     // The Airy 180 semi-major and semi-minor axes used for OSGB36 (m)
-    let a: f32 = 6377563.396;
-    let b: f32 = 6356256.909;
+    let a: f64 = 6377563.396;
+    let b: f64 = 6356256.909;
     // Scale factor on the central meridian
-    let F0: f32 = 0.9996012717;
+    let F0: f64 = 0.9996012717;
     // Latitude of true origin (radians)
-    let lat0: f32 = 49. * pi / 180.;
+    let lat0: f64 = 49. * pi / 180.;
     // Longtitude of true origin and central meridian (radians)
-    let lon0: f32 = -2. * pi / 180.;
+    let lon0: f64 = -2. * pi / 180.;
     // Northing & easting of true origin (m)
     let N0 = -100000.;
     let E0 = 400000.;
@@ -230,47 +231,47 @@ pub extern fn convert_lonlat(input_e: i32, input_n: i32) -> (f32, f32) {
 
     let mut lat = lat0;
     let mut M = 0.0;
-    while (input_n as f32 - N0 - M) >= 0.00001 {
-        lat = (input_n as f32 - N0 - M) / (a * F0) + lat;
-        let M1 = (1. + n + (5. / 4.) * n.powf(3.)
-            + (5. / 4.) * n.powf(3.)) * (lat - lat0);
-        let M2 = (3. * n + 3. * n.powf(2.) + (21. / 8.)
-            * n.powf(3.)) * (lat - lat0).sin() * (lat + lat0).cos();
-        let M3 = ((15. / 8.) * n.powf(2.) + (15. / 8.) * n.powf(3.))
+    while (input_n as f64 - N0 - M) >= 0.00001 {
+        lat = (input_n as f64 - N0 - M) / (a * F0) + lat;
+        let M1 = (1. + n + (5. / 4.) * n.powi(3)
+            + (5. / 4.) * n.powi(3)) * (lat - lat0);
+        let M2 = (3. * n + 3. * n.powi(2) + (21. / 8.)
+            * n.powi(3)) * (lat - lat0).sin() * (lat + lat0).cos();
+        let M3 = ((15. / 8.) * n.powi(2) + (15. / 8.) * n.powi(3))
             * (2. * (lat - lat0)).sin() * (2. * (lat + lat0)).cos();
-        let M4 = (35. / 24.) * n.powf(3.) * (3. * (lat - lat0)).sin()
+        let M4 = (35. / 24.) * n.powi(3) * (3. * (lat - lat0)).sin()
             * (3. * (lat + lat0)).cos();
         // Meridional arc!
         M = b * F0 * (M1 - M2 + M3 - M4);
     }
     // Transverse radius of curvature
-    let nu = a * F0 / (1. - e2 * lat.sin().powf(2.)).sqrt();
+    let nu = a * F0 / (1. - e2 * lat.sin().powi(2)).sqrt();
     // Meridional radius of curvature
-    let rho = a * F0 * (1. - e2) * (1. - e2 * lat.sin().powf(2.)).powf(-1.5);
+    let rho = a * F0 * (1. - e2) * (1. - e2 * lat.sin().powi(2)).powf(-1.5);
     let eta2 = nu / rho - 1.;
 
     let secLat = 1. / lat.cos();
     let VII = lat.tan() / (2. * rho * nu);
-    let VIII = lat.tan() / (24. * rho * nu.powf(3.))
-        * (5. + 3. * lat.tan().powf(2.)
-        + eta2 - 9. * lat.tan().powf(2.) * eta2);
-    let IX = lat.tan() / (720. * rho * nu.powf(5.))
-        * (61. + 90. * lat.tan().powf(2.)
-        + 45. * lat.tan().powf(4.));
+    let VIII = lat.tan() / (24. * rho * nu.powi(3))
+        * (5. + 3. * lat.tan().powi(2)
+        + eta2 - 9. * lat.tan().powi(2) * eta2);
+    let IX = lat.tan() / (720. * rho * nu.powi(5))
+        * (61. + 90. * lat.tan().powi(2)
+        + 45. * lat.tan().powi(4));
     let X = secLat / nu;
-    let XI = secLat / (6. * nu.powf(3.)) * (nu / rho + 2. * lat.tan().powf(2.));
-    let XII = secLat / (120. * nu.powf(5.))
-        * (5. + 28. * lat.tan().powf(2.) + 24.
-        * lat.tan().powf(4.));
-    let XIIA = secLat / (5040. * nu.powf(7.))
-        * (61. + 662. * lat.tan().powf(2.) + 1320.
-        * lat.tan().powf(4.) + 720. * lat.tan().powf(6.));
-    let dE = input_e as f32 - E0;
+    let XI = secLat / (6. * nu.powi(3)) * (nu / rho + 2. * lat.tan().powi(2));
+    let XII = secLat / (120. * nu.powi(5))
+        * (5. + 28. * lat.tan().powi(2) + 24.
+        * lat.tan().powi(4));
+    let XIIA = secLat / (5040. * nu.powi(7))
+        * (61. + 662. * lat.tan().powi(2) + 1320.
+        * lat.tan().powi(4) + 720. * lat.tan().powi(6));
+    let dE = input_e as f64 - E0;
     //  These are on the wrong ellipsoid currently: Airy1830 (Denoted by _1)
-    let lat_1 = lat - VII * dE.powf(2.)
-        + VIII * dE.powf(4.) - IX * dE.powf(6.);
-    let lon_1 = lon0 + X * dE - XI * dE.powf(3.)
-        + XII * dE.powf(5.) - XIIA * dE.powf(7.);
+    let lat_1 = lat - VII * dE.powi(2)
+        + VIII * dE.powi(4) - IX * dE.powi(6);
+    let lon_1 = lon0 + X * dE - XI * dE.powi(3)
+        + XII * dE.powi(5) - XIIA * dE.powi(7);
 
     // We Want to convert to the GRS80 ellipsoid
     // First, convert to cartesian from spherical polar coordinates
@@ -280,8 +281,8 @@ pub extern fn convert_lonlat(input_e: i32, input_n: i32) -> (f32, f32) {
     let z_1 = ((1. - e2) * nu / F0 + H) * lat_1.sin();
 
     // Perform Helmert transform (to go between Airy 1830 (_1) and GRS80 (_2))
-    let ten: f32 = 10.;
-    let s = -20.4894 * ten.powf(-6.); // The scale factor -1
+    let ten: f64 = 10.;
+    let s = -20.4894 * ten.powi(-6); // The scale factor -1
     let tx = 446.448;
     let ty = -125.157;
     let tz = 542.060; // The translations along x,y,z axes respectively
@@ -300,15 +301,15 @@ pub extern fn convert_lonlat(input_e: i32, input_n: i32) -> (f32, f32) {
     let a_2 = 6378137.00;
     let b_2 = 6356752.3141; // The GSR80 semi-major and semi-minor axes used for WGS84(m)
     let e2_2 = 1. - (b_2 * b_2) / (a_2 * a_2); // The eccentricity of the GRS80 ellipsoid
-    let p = (x_2.powf(2.) + y_2.powf(2.)).sqrt();
+    let p = (x_2.powi(2) + y_2.powi(2)).sqrt();
 
     // Lat is obtained by iterative procedure
     let mut lat = z_2.atan2((p * (1. - e2_2))); // Initial value
     let mut latold = 2. *pi;
-    let mut nu_2: f32;
-    while (lat - latold).abs() > ten.powf(-16.) {
+    let mut nu_2: f64;
+    while (lat - latold).abs() > ten.powi(-16) {
         mem::swap(&mut lat, &mut latold);
-        nu_2 = a_2 / (1. - e2_2 * latold.sin().powf(2.)).sqrt();
+        nu_2 = a_2 / (1. - e2_2 * latold.sin().powi(2)).sqrt();
         lat = (z_2 + e2_2 * nu_2 * latold.sin()).atan2(p);
     }
 
@@ -333,7 +334,7 @@ pub extern fn convert_vec_c(lon: Array, lat: Array) -> Array {
         .cloned());
     // carry out the conversion 
     let result = orig
-        .map(|elem| convert_bng(elem.0, elem.1));
+        .map(|elem| convert_bng(elem.0 as f64, elem.1 as f64));
     // convert back to vector of unsigned integer Tuples
     let nvec = result
         .map(|ints| IntTuple { a: ints.0 as u32, b: ints.1 as u32 })
@@ -366,7 +367,7 @@ pub extern fn convert_to_bng(lon: Array, lat: Array) -> Array {
         let chunk = chunk.to_owned();
         let g = thread::spawn(move || chunk
             .into_iter()
-            .map(|elem| convert_bng(elem.0, elem.1))
+            .map(|elem| convert_bng(elem.0 as f64, elem.1 as f64))
             .collect());
         guards.push(g);
     }
@@ -393,7 +394,7 @@ pub extern fn convert_to_lonlat(eastings: Array, northings: Array) -> Array {
         .cloned())
     .collect();
 
-    let mut guards: Vec<JoinHandle<Vec<(f32, f32)>>> = vec!();
+    let mut guards: Vec<JoinHandle<Vec<(f64, f64)>>> = vec!();
     // split into slices
     let mut size = orig.len() / NUMTHREADS;
     if orig.len() % NUMTHREADS > 0 { size += 1; }
@@ -519,9 +520,9 @@ mod tests {
             398915,
             521545,
             604932,
-            188805, // BNG says 188804
+            188804, // BNG says 188804
             574082,
-            61932, // BNG says 61931
+            61931, // BNG says 61931
             523242,
             517193,
             515004,
@@ -555,7 +556,7 @@ mod tests {
 
     #[test]
     fn test_lonlat_conversion() {
-        assert_eq!((-0.328248, 51.44534), convert_lonlat(516276, 173141));
+        assert_eq!((-0.32824799370716407, 51.44534026616287), convert_lonlat(516276, 173141));
     }
 
     #[test]
