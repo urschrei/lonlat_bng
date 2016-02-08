93da1b0b8f013280fa9334c347816b3d3125ef5a
Stephan Hügel <stephan.hugel.12@ucl.ac.uk>
2016-02-08 14:06:54 +0000

    Update list of OSTN02-enabled functions

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 #![doc(html_root_url = "https://urschrei.github.io/lonlat_bng/")]
 //! The `lonlat_bng` crate provides functions that convert decimal longitude
-//! and latitude coordinates into [British National Grid](https://en.wikipedia.org/wiki/Ordnance_Survey_National_Grid) coordinates, and vice versa.
+//! and latitude coordinates into [British National Grid](https://en.wikipedia.org/wiki/Ordnance_Survey_National_Grid) coordinates, and vice versa.  
 //! This library makes use of the [OSTN02](https://www.ordnancesurvey.co.uk/business-and-government/help-and-support/navigation-technology/os-net/surveying.html) transformations
 //! for the following functions:
 //!
@@ -8,6 +8,8 @@
 //! - [`convert_ETRS89_to_OSGB36`](fn.convert_ETRS89_to_OSGB36.html)
 //! - [`convert_to_osgb36_threaded`](fn.convert_to_osgb36_threaded.html)
 //! - [`convert_to_osgb36_threaded_vec`](fn.convert_to_osgb_threaded_vec.html)
+//! - [`convert_ETRS89_to_OSGB36_threaded`](fn.convert_ETRS89_to_OSGB36_threaded.html)
+//! - [`convert_ETRS89_to_OSGB36_threaded_vec`](fn.convert_ETRS89_to_OSGB36_threaded_vec.html)
 //!
 //! These functions transform input longitude and latitude coordinates to OSGB36 Eastings and Northings with high accuracy, and are suitable for use in surveying and construction. Please run your own tests, though.
 //! **Note that `lon`, `lat` coordinates outside the UK bounding box will be transformed to `(9999, 9999)`, which cannot be mapped.**
