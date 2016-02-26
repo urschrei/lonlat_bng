190237fb00ac8dc622cd92cf2ac8efb0a5334ca6
Stephan Hügel <urschrei@gmail.com>
2016-02-26 20:35:42 +0000

    Allow arbitrary "static" rows and values

--- aggregate_bench.py
+++ aggregate_bench.py
@@ -16,22 +16,28 @@ pattern = re.compile(
     r"bench:\s+([0-9,]*)\D+([0-9,]*)"
 )
 
-def dump_benchmark(pattern, filepath=None, headers=None, dep_var=None):
+def dump_benchmark(pattern, filepath=None, headers=None, dep_var=None, **kwargs):
     """ If I have to append benchmark output to a CSV once more I'm going
     to drown the world in a bath of fire. This should just work.
     Customise with your own output path and header row.
     dep_var is an optional dependent variable.
     """
     if not filepath:
-        filepath = "benches/measurements.csv"
+        filepath = "measurements.csv"
     if not headers:
         headers = ['time', 'error']
     # run cargo bench in cwd, capture output
     result = re.search(pattern, check_output(["cargo", "bench"]))
     output = [int(group.translate(None, ',')) for group in result.groups()]
+    # this one's special because wtf are we measuring without a dependent variable
     if dep_var:
         headers.append("dependent_variable")
         output.append(dep_var)
+    # anything else will get written as a CSV header row and value
+    # nothing prevents you from writing rows that don't have a header
+    for k, v in kwargs.items():
+        headers.append(k),
+        output.append(v)
     # check that path and file exist, or create them
     path_wrangle(filepath, headers)
     # get rid of nasty commas, convert to int, and write
@@ -58,4 +64,4 @@ if __name__ == "__main__":
     # So brittle. Shhh.
     if sys.argv[1] is not None:
         dep_var = sys.argv[1]
-    dump_benchmark(pattern, dep_var=dep_var)
+    dump_benchmark(pattern, filepath="benches/measurements.csv", dep_var=dep_var)
