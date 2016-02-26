237483267fce790df1deb2b4e368edc1fc65321e
Stephan Hügel <urschrei@gmail.com>
2016-02-26 10:15:59 +0000

    Move point generation out of benchmark generation

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -10,14 +10,13 @@ use rand::distributions::{IndependentSample, Range};
 
 #[bench]
 fn bench_threads(b: &mut Bencher) {
-    let num_coords = 10000;
+    let num_coords = 100000;
     let between_lon = Range::new(-6.379880, 1.768960);
     let between_lat = Range::new(49.871159, 55.811741);
     let mut rng = rand::thread_rng();
-
+    let mut lon_vec = vec![between_lon.ind_sample(&mut rng); num_coords];
+    let mut lat_vec = vec![between_lat.ind_sample(&mut rng); num_coords];
     b.iter(||{
-        let mut lon_vec = vec![between_lon.ind_sample(&mut rng); num_coords];
-        let mut lat_vec = vec![between_lat.ind_sample(&mut rng); num_coords];
         convert_to_bng_threaded_vec(
             &mut lon_vec,
             &mut lat_vec
