8ac02c505f6ff53ca13f90f5fab286ed7c11ec26
Stephan Hügel <urschrei@gmail.com>
2016-06-10 16:56:02 +0200

    Add preinstall script

---
+++ ci/preinstall.sh
@@ -0,0 +1,8 @@
+  if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+    brew update
+    brew install python
+    pip install virtualenv
+    virtualenv venv -p python
+    source venv/bin/activate
+    pip install pip --upgrade
+fi
