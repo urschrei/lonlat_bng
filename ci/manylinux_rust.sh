ecc11b9db6fca3d8525519a4bf51e227b55b4e5d
Stephan Hügel <urschrei@gmail.com>
2016-06-13 11:49:33 +0200

    Fix Cargo and output locations in Docker

--- ci/manylinux_rust.sh
+++ ci/manylinux_rust.sh
@@ -18,7 +18,7 @@ install_rustup() {
 
 # Generate artifacts for release
 mk_artifacts() {
-    cargo build --target $TARGET --release
+    cargo build --manifest-path=/io --target $TARGET --release
 }
 
 mk_tarball() {
@@ -28,7 +28,7 @@ mk_tarball() {
 
     # TODO update this part to copy the artifacts that make sense for your project
     # NOTE All Cargo build artifacts will be under the 'target/$TARGET/{debug,release}'
-    for lib in target/$TARGET/release/liblonlat_bng.*; do
+    for lib in /io/target/$TARGET/release/liblonlat_bng.*; do
         strip -s $lib
     done
     cp target/$TARGET/release/liblonlat_bng.* $td
